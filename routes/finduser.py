5478562497c94c36faf2ce5e515aaa6262d20e1d
Heainz Manoj <heainz.manoj@gmail.com>
2024-03-12 17:49:42 -0400

    :(

--- routes/finduser.py
+++ routes/finduser.py
@@ -1,6 +1,15 @@
 from __main__ import app
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import sqlite3
+
 
 @app.route('/user/<name>', methods=['GET'])
 def get_user(name):
-    return None
+    connection = sqlite3.connect("database.db")
+    users = connection.execute("SELECT * FROM users WHERE name = " + name).fetchone()
+    connection.close()
+
+    
+    return jsonify({
+        "phoneNumber": users[0] 
+    })
